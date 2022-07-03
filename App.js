c9f456b46d8c910411e7c17162fbb65f2b71c66c
RichardBrowning <mi888uzl@outlook.com>
2022-07-03 08:07:00 +0800

    working edition, ryan and porsche does not work

--- App.js
+++ App.js
@@ -1,22 +1,28 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect, useRef } from 'react'
 import { useFrame, Canvas } from '@react-three/fiber/native'
-import { useGLTF, Environment } from '@react-three/drei/native'
+import { useGLTF, Environment, useAnimations } from '@react-three/drei/native'
 import iphoneModelPath from './assets/iphone.glb'
+import porscheModelPath from './assets/porsche.glb'
+import ryanPath from './assets/Ryan.glb'
+import userAvPath from './assets/userAvatarAnimation.glb'
+import bananaPath from './assets/banana.glb'
 
 function Model({ url, ...rest }) {
-  const { scene } = useGLTF(url)
+  const group = useRef()
+  const { scene, nodes, materials, animations } = useGLTF(url)
   useFrame(() => (scene.rotation.y += 0.01))
+  const { actions } = useAnimations(animations, group)
   return <primitive {...rest} object={scene} />
 }
 
 export default function App() {
   return (
-    <Canvas gl={{ physicallyCorrectLights: true }} camera={{ position: [-6, 0, 16], fov: 36 }}>
+    <Canvas legacy={true} gl={{ physicallyCorrectLights: true }} camera={{ position: [-10, 15, 20], fov: 35, zoom:0.5}}>
       <color attach="background" args={[0xe2f4df]} />
       <ambientLight />
       <directionalLight intensity={1.1} position={[0.5, 0, 0.866]} />
       <directionalLight intensity={0.8} position={[-6, 2, 2]} />
-      <Suspense>
+      <Suspense fallback={null}>
         <Environment preset="park" />
         <Model url={iphoneModelPath} />
       </Suspense>
